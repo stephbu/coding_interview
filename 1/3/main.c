471940d49b2ee316e3fdcb3aad3071cd968cdd34
stephbu <stephbu@gmail.com>
2015-05-11 21:57:49 -0700

    Problem 1.3

---
+++ 1/3/main.c
@@ -0,0 +1,74 @@
+#define NULL 0
+#include <string.h>
+#include <stdio.h>
+
+typedef enum { false, true } bool;
+
+bool IsPermutation(char* src, char* test) {
+	
+    if(NULL == src || NULL == test) {
+    	return false;
+    }
+
+    int srcLength = strlen(src);
+    int testLength = strlen(test);
+
+    if(srcLength != testLength) {
+        return false;
+    }
+    
+    int map[256];
+    for(int index = 0; index < 256; index++) {
+        map[index] = 0;
+    }
+    
+    // set the map
+    for(int index = 0; index < srcLength; index++) {
+        map[src[index]]++;
+    }
+    
+    // test against map
+    for(int index = 0; index < testLength; index++) {
+        if(map[test[index]] == 0) {
+            return false;
+        }
+        map[test[index]]--;
+    }
+
+    return true;    
+}
+
+int main() {
+	char test1[] = "foo";
+	char test2[] = "fof";
+	char test3[] = "sets";
+    char test4[] = "tess";
+	char test5[] = "bad";
+    char test6[] = "length";
+	
+    bool result;
+    
+	result = IsPermutation(test1,test2);
+    if(result == true) {
+    	printf("%s is permutation of %s\r\n", test1, test2);    
+    }
+    else {
+    	printf("%s is not permutation of %s\r\n", test1, test2);    
+    }
+    
+	result = IsPermutation(test3,test4);
+    if(result == true) {
+    	printf("%s is permutation of %s\r\n", test3, test4);    
+    }
+    else {
+    	printf("%s is not permutation of %s\r\n", test3, test4);    
+    }
+    
+	result = IsPermutation(test5,test6);
+    if(result == true) {
+    	printf("%s is permutation of %s\r\n", test5, test6);    
+    }
+    else {
+    	printf("%s is not permutation of %s\r\n", test5, test6);    
+    }
+}
